41c110e4d653c551c798ee14694c368d5276af39
jason <j.solorzano1910@gmail.com>
2024-10-07 18:54:58 -0600

    Primer commit semana 3

---
+++ Tienda_M/src/main/java/Tienda/com/TiendaMApplication.java
@@ -0,0 +1,13 @@
+package Tienda.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMApplication.class, args);
+	}
+
+}
